0bc13787f95477bb4aa9dedde43f4b87275df2f4
aitmalika <malikasaih21@gmail.com>
2023-09-15 05:01:27 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -6,19 +6,17 @@
  * main - Entry point
  * Return: Alwayws 0 (Success)
  */
-
 int main(void)
-
 {
 	int d;
 	char low;
-	
+
 	for (d = '0'; d <= '9'; d++)
 	putchar(d);
-	
+
 	for (low = 'a'; low <= 'f'; low++)
 	putchar(low);
 	putchar('\n');
-	
+
 	return (0);
 }
